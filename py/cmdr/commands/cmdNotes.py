a85c02b994ddad6c591c9f88b1566d73cf690ab3
Ozgur Yuksel <ozgur@insequor.com>
2017-09-13 21:06:58 +0300

    Initial command to take notes
    This is the first version to intoduce the commands, no execution

---
+++ py/cmdr/commands/cmdNotes.py
@@ -0,0 +1,65 @@
+#=============================================================================
+#=== Ozgur Aydin Yuksel, 2014 (c)
+#=============================================================================
+
+#standard
+
+#thirdparty
+import webbrowser
+
+#internal
+
+__doc__ = '''
+'''
+#=============================================================================
+#===Configuration
+#=============================================================================
+
+
+#=============================================================================
+#===
+#=============================================================================
+class Command:
+    author = 'Ozgur Aydin Yuksel'
+    info = '''Short cuts for taking notes'''
+    
+    
+    #--
+    def __init__(self, ui, wx, manager):
+        self.ui = ui
+        self.wx = wx
+        self.manager = manager
+        
+        self.__functions = {
+              'note': self.__note
+            , 'life lesson': self.__life_lesson
+            , 'shared note': self.__shared_note 
+        }
+        
+        self.names = self.__functions.keys()
+        
+    options = []
+        
+    def __note(self, option):
+        print 'Note: ', option
+        
+    def __life_lesson(self, option):
+        print 'Life Lesson: ', option
+
+    def __shared_note(self, option):
+        print 'Shared Note: ', option
+        
+    #--
+    def execute(self, name, option):
+        option = option.strip()
+        self.__functions[name](option)
+        return True
+
+#=============================================================================
+#===
+#=============================================================================
+if __name__ == '__main__':
+    pass
+    
+    
+    
